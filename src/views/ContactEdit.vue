47a227457c291cce050b7f78c1e9a79998b5f68c
tinhto123 <Tinhck23v7k532@vlvh.ctu.edu.vn>
2025-03-17 17:10:15 +0700

    Sửa lỗi ở ContactEdit.vue

--- src/views/ContactEdit.vue
+++ src/views/ContactEdit.vue
@@ -31,7 +31,6 @@ export default {
         this.contact = await ContactService.get(id);
       } catch (error) {
         console.log(error);
-        // Chuyển sang trang NotFound đồng thời giữ cho URL không đổi
         this.$router.push({
           name: "notfound",
           params: {
